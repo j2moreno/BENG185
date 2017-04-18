d44d2a370fdf098fc2d68238e6375440e674582b
j2moreno <leo@Leonardos-MacBook-Pro.local>
2017-04-17 23:35:34 -0700

    Week #2 Exercise (Tuesday)
    Week #2 Exercise (Tuesday)

--- iterateThroughTBL.py
+++ iterateThroughTBL.py
@@ -1,19 +1,24 @@
 import os
-rootdir = 'dirtree/'
 
-#print [x[0] for x in os.walk(rootdir)]
+#begins at the given directory 
+rootdir = 'dirtree/' 
+
+#gets all file names within the rootdir 
 array =  next(os.walk(rootdir))[1]
 
 i = 0
-
+#recursively go through all files and get only .tbl files
 for subdir, dirs, files in os.walk(rootdir):
        for file in files:
                  if file.endswith(".tbl"):
-                                
+
                                 openedfile = open(os.path.join(subdir, file), 'r')
+                                #keeps reading line until it gets to approipiate column
                                 data = openedfile.readline()
                                 data2 = openedfile.readline()
                                 data3 = openedfile.readline()
+                                
+                                #prints name of file and its value
                                 print array[i] + '\t' +  data3.split('\t')[3]
                                 i +=1
                                 break
