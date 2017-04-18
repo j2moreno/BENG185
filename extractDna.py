46190539f64f561ac8e422c9472d045e54a73ec8
j2moreno <leo@Leonardos-MacBook-Pro.local>
2017-04-17 23:31:36 -0700

    Week 2 Exercise (Tuesday)
    Week 2 Exercise (Tuesday)

--- extractDna.py
+++ extractDna.py
@@ -1,15 +1,15 @@
 import sys
 
+#function that returns the reverse compliment of the string given
 def reverseComp(text):
-  reverseStack = []
-  for i in range(len(text)):
+
+  reverseStack = [] #holds the reverse compliment of the string in the form of a stack 
+
+  for i in range(len(text)): # for every letter in text
     if text[i] == "A":
-      ##text.replace(text[i], 'T')
       reverseStack.append('T')
     elif text[i] == 'T':
-      #text.replace(text[i], 'A')
       reverseStack.append('A')
-      
     elif text[i] == 'C':
       reverseStack.append('G')
     elif text[i] == 'G':
@@ -17,29 +17,39 @@ def reverseComp(text):
 
   reverseComplement = []
   for i in range(len(reverseStack)):
-    reverseComplement.append(reverseStack.pop())
+    #pops from stack to get reverse compliement 
+    reverseComplement.append(reverseStack.pop()) 
+
+   #returns reverse compliement in the form of a string  
   return("".join(str(x) for x in reverseComplement))
 
+#function that prints DNA sequence and continues the sequence
+#every 70 nuclotides
 def insert_newlines(string, every=70):
     lines = []
+    
     for i in xrange(0, len(string), every):
         lines.append(string[i:i+every])
     return '\n'.join(lines)
 
-filename = sys.argv[1]
+filename = sys.argv[1] # argument 1 which is for the Protein Table
 file = open(filename, 'r')
 
 data = file.readlines()
-start = []
-end = []
-reverse = []
-locus = []
-tag = []
-name = []
+start = [] # holds all start positions 
+end = []# holds all end positions 
+reverse = [] #holds whether the gene is on the forward or reverse strand
+locus = [] #holds locus's 
+tag = [] #holds the locus's tags
+name = [] #holds the name of the gene
 
+
+#appends the appropiate elements to each array
 for element in data:
-  element = element.replace('\n', '')
-  element = element.split('\t')
+  element = element.replace('\n', '') #gets rid of newline character
+  element = element.split('\t')# gets rid of tabs
+
+  #the appending step for each 
   start.append(element[2])
   end.append(element[3])
   reverse.append(element[4])
@@ -47,21 +57,22 @@ for element in data:
   tag.append(element[7])
   name.append(element[8])
 
-filename2 = sys.argv[2]
-DNA = ""
+filename2 = sys.argv[2] #reads genome file 
+DNA = "" #string that builds to be the whole genome
 with open(filename2) as GENOME:
     for line in GENOME:
         if line.find(">") != 0: #skip the header of the genome fa file
             DNA += line.strip() #concatenate genome sequences into a 0-indexing string
 
-
+#goes through appropiate fields and prints out name of gene, 
+#locus title, locus tag, followed by the sequence 
 for i in range(1, len(start)):
-  if reverse[i] == '-':
+  if reverse[i] == '-': #checks if it is on the reverse strand 
     seq = reverseComp(DNA[int(start[i])-1: int(end[i])])
     print '>' + name[i] + '|' + locus[i] + '|' + tag[i]
     print insert_newlines(seq)
     
-  else:
+  else: #checks if the gene is on the froward strand 
     print '>' + name[i] + '|' + locus[i] + '|' + tag[i]
     seq = DNA[int(start[i])-1: int(end[i])]
     print insert_newlines(seq)
